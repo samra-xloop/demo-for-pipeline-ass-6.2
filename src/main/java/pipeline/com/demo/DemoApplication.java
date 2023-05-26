facbeee275507a3f60bae150dfaf11b6df2f5329
Samra Almas 2211-015-KHI-CND <119280481+samra-xloop@users.noreply.github.com>
2023-05-26 23:12:05 +0500

    first commit

---
+++ src/main/java/pipeline/com/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package pipeline.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String sayHello(){
+		return "Hello";
+	}
+
+
+}
